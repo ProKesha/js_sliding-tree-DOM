f85b94692d003a51c9caf7096d175f1926d709bc
Dmytro Popov <lort20178@gmail.com>
2025-10-28 21:31:06 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('.tree li');
+
+items.forEach((li) => {
+  const childUl = li.querySelector('ul');
+  const text = li.firstChild.textContent.trim();
+
+  if (text && childUl) {
+    const span = document.createElement('span');
+
+    span.textContent = text;
+    li.firstChild.replaceWith(span);
+
+    childUl.style.overflow = 'hidden';
+    childUl.style.maxHeight = childUl.scrollHeight + 'px';
+    childUl.style.transition = 'max-height 0.3 ease';
+
+    span.addEventListener('click', () => {
+      if (childUl.style.maxHeight === '0px') {
+        childUl.style.maxHeight = childUl.scrollHeight + 'px';
+      } else {
+        childUl.style.maxHeight = '0px';
+      }
+    });
+  }
+});
